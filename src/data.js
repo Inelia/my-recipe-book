69d9f9d2fc615c10ae4fc27a5cadcfd2c53a8192
Inelia <agniel.marie@gmail.com>
2022-04-01 12:28:30 +0200

    populate recipe

--- src/data.js
+++ src/data.js
@@ -7,12 +7,35 @@ export default{
             level:"Facile",
             servs:"8",
             prepTemp:"1h",
-            cookingTemp:"",
-            stars:"",
-            cost:"",
-            ingredients:"",
-            img:"",
-            instructions:"",
+            cookingTemp:"30min",
+            stars:"5/5",
+            cost:"€",
+            ingredients:[
+                "Pour la pâte",
+                "250 grammes de farine",
+                "1 oeuf battu ",
+                "50 grammes de sucre glace ",
+                "45 grammes de poudre d'amande ",
+                "135 grammes de beurre très froid ",
+                "Pour la compote ",
+                "4 pêches bien mûres ",
+                "170 grammes de fraises ",
+                "1 c. à soupe d'eau de fleur d'oranger ",
+                "2 c. à soupe d'eau ",
+                "Pour la garniture ",
+                "250 grammes de fraises ",
+                "quelques myrtilles",
+                "1 c. à soupe d'amandes ",
+                "un peu ",
+                "de sucre glace",
+            ],
+            img:"https://img.cuisineaz.com/660x660/2017/06/07/i128107-.webp",
+            instructions:[
+                "Pour la pâte, mélangez la farine avec le sucre glace, la poudre d'amande dans un saladier. Ajoutez le beurre très froid. Sablez le tout du bout des doigts. Ajoutez l'oeuf. Mélangez (toujours avec les doigts) juste assez pour former une boule. La pâte colle encore un peu aux mains c'est normal. Enveloppez-la dans du film alimentaire et placez-la au frigo pour au moins une heure.",
+                "Pour la compote pêches et fraises, pelez les pêches, ôtez les noyaux. Coupez la chair en morceaux et placez-les dans une casserole. Lavez, équeutez et séchez les fraises. Coupez les plus grosses en deux. Ajoutez-les aux pêches. Versez la fleur d'oranger et l'eau. Portez sur feu assez vif. Remuez régulièrement. Comptez environ 20 minutes de cuisson, la compote doit s'assécher. Quand elle a la bonne consistance, mixez-la finement avec votre mixeur plongeant. Réservez. Faites préchauffer votre four à 180°C.",
+                "Beurrez et farinez un moule à tarte. Coupez la pâte en deux. Prenez-en une moitié et étalez-la finement avec un rouleau à pâtisserie votre plan de travail fariné. Garnissez-en le moule. Ôtez l'excédent de pâte. Placez une feuille de papier sulfurisé sur le fond de tarte, recouvrez de légumes secs ou de poids de cuisson. Enfournez et laissez cuire 15 minutes. Ôtez alors les poids et le papier, poursuivez la cuisson pendant 10 minutes. Sortez le plat du four et laissez-la pâte refroidir.",
+                "Lavez, séchez équeutez les fraises qui servent de garniture. Coupez-les en deux )ou plus si elles sont très grosses). Lavez et séchez les myrtilles. Concassez les amandes (pas trop finement). Au moment de servir le dessert, étalez la compote sur le fond de tarte. Disposez ensuite les fraises en rosace. Répartissez les myrtilles et les amandes. Saupoudrez légèrement de sucre glace. Éventuellement, décorez aussi avec quelques petites feuilles de menthe."
+            ],
         },
         {
             title:"",
@@ -41,16 +64,5 @@ export default{
             instructions:"",
         }
 
-    ],
-    card:[
-        {
-        title:""
-        },
-        {
-        title:""
-        },
-        {
-        title:""
-        }
     ]
 }
