dbb9a1f56a4d1247aec4c216bedd690e60b43e5c
LauffenburgerSergei <lauffenburger@live.fr>
2022-04-01 11:38:58 +0200

    create card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,12 @@ import './Card.css'
 
 function Card(props) {
   return (
-    <article>Card</article>
+    <article className="card">
+        <img src={props.img} alt={props.alt} className="card__img" />
+        <h3 className='card__title'></h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+    </article>
   )
 }
 
