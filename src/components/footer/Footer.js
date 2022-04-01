d3720584dd48b5c129659850c97be6ed3f5bc0b4
Inelia <agniel.marie@gmail.com>
2022-04-01 09:43:58 +0200

    creation footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer><a href="https://github.com/LauffenburgerSergei">Sergei Lauffenburger</a>|<a href="https://github.com/Inelia">Marie Agniel</a></footer>
+  )
+}
+
+export default Footer
