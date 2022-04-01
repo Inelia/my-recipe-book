de64f94661405fa6aabe6f47a0ae8131988a2002
Inelia <agniel.marie@gmail.com>
2022-04-01 09:36:18 +0200

    creation recipe + data.js prep de l'array recipe

---
+++ src/data.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default{
+    recipes:[
+        {
+            title:"",
+            description:"",
+            level:"",
+            servs:"",
+            prepTemp:"",
+            cookingTemp:"",
+            stars:"",
+            cost:"",
+            ingredients:"",
+            img:"",
+            instructions:"",
+        }
+    ]
+}
---
+++ src/pages/RecipeDetails/RecipeDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecipeDetails() {
+  return (
+    <main>
+        img
+    </main>
+  )
+}
+
+export default RecipeDetails
