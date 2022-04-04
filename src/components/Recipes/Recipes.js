e1eaafcd37de93ceb07c84dcf48a024eb5f40e85
Marie Agniel <agniel.marie@gmail.com>
2022-04-04 09:40:06 +0200

    prep create recipe.js

---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import	'./Recipes.css'
+
+function Recipes(props) {
+  return (
+    <article className='recipe__details'>
+      <h3 className='recipe__title'>{props.title}</h3>
+      <img src={props.img} alt={props.title} className="recipe__img" />
+      <section className='recipe__info'>
+      <section className='recipe__timePrep'>
+        <i class="fa-solid fa-timer"></i>
+      <p>{props.prepTemp} + {props.cookingTemp}</p>
+      </section>
+      <section className='recipe__difficulty'>
+        <i class="fa-solid fa-hat-chef"></i>
+        <p>{props.level}</p>
+      </section>
+      <section className='recipe__stars'>
+        <i class="fa-solid fa-hat-chef"></i>
+        <p>{props.stars}</p>
+      </section>
+      </section>
+    </article>
+  )
+}
+
+export default Recipes
