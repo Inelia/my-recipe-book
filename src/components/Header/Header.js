e99b3b1da542380541e4278f5cb8266998beb74e
sergei lauffenburger <lauffenburger@live.fr>
2022-04-04 10:15:20 +0200

    header mise en page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,17 @@ import "./Header.css"
 function Header() {
   return (
     <header>
-        <div>
+        
             <h1>My Recipe Book</h1>
-        </div>
-        <nav>
+        
+        <nav className='navigation'>
                 <a href="accueil">Accueil</a>
                 
                 <a href="starters">Entrées</a>
                 
                 <a href="mains">Plats</a>
                 
-                <a href="desserts">Déssert</a>
+                <a href="desserts">Dessert</a>
                
         </nav>
     </header>
